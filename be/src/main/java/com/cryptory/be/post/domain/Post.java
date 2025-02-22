ed96849072e838df4f34d93b7b04802aff4675b0
HyungSuhOh <43169705+ohhyungsuh@users.noreply.github.com>
2025-02-22 17:53:28 +0900

    refactor: 코인 엔티티 병합에 따른 연관관계 맵핑 주석 해제

--- be/src/main/java/com/cryptory/be/post/domain/Post.java
+++ be/src/main/java/com/cryptory/be/post/domain/Post.java
@@ -33,10 +33,9 @@ public class Post extends BaseTimeEntity {
 
     private boolean isDeleted;
 
-    // todo: 코인 엔티티 추가되면 주석 해제
-//    @ManyToOne(fetch = FetchType.LAZY)
-//    @JoinColumn(name = "coin_id")
-//    private Coin coin;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "coin_id")
+    private Coin coin;
 
     @Builder
     public Post(String title, String body, User user) {
