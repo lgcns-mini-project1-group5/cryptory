852b2155a4ca53eaeaa10ec239902d3f65cdb7d8
HyungSuhOh <43169705+ohhyungsuh@users.noreply.github.com>
2025-02-24 00:49:48 +0900

    refactor: 요청 어노테이션 수정 및 응답 Dto 수정

--- be/src/main/java/com/cryptory/be/issue/domain/IssueComment.java
+++ be/src/main/java/com/cryptory/be/issue/domain/IssueComment.java
@@ -42,10 +42,11 @@ public class IssueComment extends BaseTimeEntity {
     private Issue issue;
     
     @Builder
-    public IssueComment(String content, User user) {
+    public IssueComment(String content, User user, Issue issue) {
         this.content = content;
         this.user = user;
-        
+        this.issue = issue;
+
         this.isDeleted = false;
     }
     
