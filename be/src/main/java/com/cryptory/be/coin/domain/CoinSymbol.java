b27e25f2bb9d927ef083b5423762221a16789612
HyungSuhOh <43169705+ohhyungsuh@users.noreply.github.com>
2025-02-23 19:22:12 +0900

    feat: 코인 심볼 enum 추가 및 응답 반환시 색상, 로고 추가

--- be/src/main/java/com/cryptory/be/coin/domain/CoinSymbol.java
+++ be/src/main/java/com/cryptory/be/coin/domain/CoinSymbol.java
@@ -19,17 +19,8 @@ public class CoinSymbol {
     private String color;
     private String logoUrl;
 
-    @OneToOne(mappedBy = "coinSymbol")
-    private Coin coin;
-
     @Builder
     public CoinSymbol(String color, String logoUrl) {
-        this.color = color;
-        this.logoUrl = logoUrl;
-    }
-
-    // TODO 초기 색깔, url 데이터는 어디서? 혹시 몰라서 관리자에서 업데이트할 수 있도록 메소드 추가
-    public void updateColorAndLogo(String color, String logoUrl) {
         this.color = color;
         this.logoUrl = logoUrl;
     }
