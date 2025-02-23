c64cb99f03c15bdf27a5b982bdea4941a9e86acd
조영상 <51320595+0ssang@users.noreply.github.com>
2025-02-24 01:39:48 +0900

    feat: 게시글 강제 삭제 기능 구현
    댓글 삭제 기능은 Comment entity, repository 생성 후 구현예정

--- be/src/main/java/com/cryptory/be/admin/service/AdminPostCommentServiceImpl.java
+++ be/src/main/java/com/cryptory/be/admin/service/AdminPostCommentServiceImpl.java
@@ -1,6 +1,12 @@
 package com.cryptory.be.admin.service;
 
+import com.cryptory.be.post.domain.Post;
+import com.cryptory.be.post.repository.PostRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 /**
  * packageName    : com.cryptory.be.admin.service
@@ -14,5 +20,21 @@ import org.springframework.stereotype.Service;
  * 2/22/25         조영상        최초 생성
  */
 @Service
+@RequiredArgsConstructor
+@Transactional
 public class AdminPostCommentServiceImpl implements AdminPostCommentService {
+    private final PostRepository postRepository;
+    //private final CommentRepository commentRepository;
+
+    @Override
+    public void deletePosts(List<Long> ids) {
+        List<Post> posts = postRepository.findAllByIdsAndNotDeleted(ids);
+        posts.forEach(Post::delete);
+    }
+
+    // TODO. Comment 엔티티와 리포지토리 생성 후 개발
+    @Override
+    public void deleteComment(Long commentId) {
+
+    }
 }
--- be/src/main/java/com/cryptory/be/post/repository/PostRepository.java
+++ be/src/main/java/com/cryptory/be/post/repository/PostRepository.java
@@ -4,6 +4,8 @@ import com.cryptory.be.post.domain.Post;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -13,4 +15,8 @@ public interface PostRepository extends JpaRepository<Post, Long> {
     Page<Post> findAllByCoinId(Long coinId, Pageable pageable);
 
     long countByCoinId(Long coinId);
+
+    //ID 목록에 해당하는 Post 중에서 isDeleted가 false인 Post만 조회
+    @Query("SELECT p FROM Post p WHERE p.id IN :ids AND p.isDeleted = false ")
+    List<Post> findAllByIdsAndNotDeleted(@Param("ids") List<Long> ids);
 }
