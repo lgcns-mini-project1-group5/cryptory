cee8d8799d40f947250aecd557f81f86e210b663
HyungSuhOh <43169705+ohhyungsuh@users.noreply.github.com>
2025-02-24 10:22:23 +0900

    refactor: 응답 API 통일하게  수정

--- be/src/main/java/com/cryptory/be/chart/controller/ChartController.java
+++ be/src/main/java/com/cryptory/be/chart/controller/ChartController.java
@@ -10,9 +10,4 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1/charts")
 public class ChartController {
 
-    // TODO 차트 목록을 업비트 API로부터 불러와 형식에 맞게 저장(처음 한 번만. 초기화. 하루마다 자동 최신화 - 이건 구현 못함)
-
-
-    // TODO 차트 목록을 조회
-
 }
--- be/src/main/java/com/cryptory/be/news/controller/NewsController.java
+++ be/src/main/java/com/cryptory/be/news/controller/NewsController.java
@@ -1,8 +1,10 @@
 package com.cryptory.be.news.controller;
 
+import com.cryptory.be.global.response.ApiResponse;
 import com.cryptory.be.news.dto.NewsDto;
 import com.cryptory.be.news.service.NewsService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,7 +20,9 @@ public class NewsController {
     private final NewsService newsService;
 
     @GetMapping
-    public ResponseEntity<List<NewsDto>> getNews() {
-        return ResponseEntity.ok(newsService.getNews());
+    public ApiResponse<NewsDto> getNews() {
+        List<NewsDto> news = newsService.getNews();
+
+        return new ApiResponse<>(HttpStatus.OK, news);
     }
 }
